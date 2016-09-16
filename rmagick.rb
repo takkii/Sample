f72b1194fffcaea693425c6438f87028809f5c0a
takkii <karuma.reason@gmail.com>
2016-09-16 18:49:45 +0900

    画像の変更.

--- rmagick.rb
+++ rmagick.rb
@@ -7,13 +7,13 @@ image2 = image.wave(amplitude=10.0, wavelength=100.0)
 
 md = Magick::Draw.new
 
-md.annotate(image2,0,0,0,0,"Ushio (Cat) is Octopass ! ") do
+md.annotate(image2,0,0,0,0,"Ushio is Cat & Octopus.") do
 
 	md.gravity = Magick::NorthWestGravity
 
 	md.pointsize = 45
-	md.fill = "blue"
-	md.stroke = "white"
+	md.fill = "green"
+	md.stroke = "blue"
 end
 
 image2.write("CatTera.png")
