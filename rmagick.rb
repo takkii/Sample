c6429c527d8b99213d3c1b70f961f8ea7bd39d68
takkii <karuma.reason@gmail.com>
2016-09-21 21:23:02 +0900

    new file add and image messge change.

--- rmagick.rb
+++ rmagick.rb
@@ -2,13 +2,11 @@ require 'RMagick'
 include Magick
 
 class Ushio
-	
 	def Ushio.cat
-
-		image = Magick::ImageList.new("octopass.png")
+          image = Magick::ImageList.new("octopass.png")
 		image2 = image.wave(amplitude=10.0, wavelength=100.0)
 		md = Magick::Draw.new
-		md.annotate(image2,0,0,0,0,"Ushio is Cat&Octopus.") do
+		md.annotate(image2,0,0,0,0,"MyCat name is Usio.") do
 			md.gravity = Magick::NorthWestGravity
 			md.pointsize = 50
 			md.fill = "green"
