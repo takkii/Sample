b8e9953b2c622a09b883827250b60850bfb336f0
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-05-13 19:13:51 +0900

    Update.

--- rmagick.rb
+++ rmagick.rb
@@ -4,7 +4,7 @@ include Magick
 
 class Ushio
 	def Ushio.cat
-          image = Magick::ImageList.new("octopass.png")
+          image = Magick::ImageList.new("CatTera.png")
 		image2 = image.wave(amplitude=10.0, wavelength=100.0)
 		md = Magick::Draw.new
 		md.annotate(image2,0,0,0,0,"MyCat name is Usio.") do
@@ -18,39 +18,3 @@ class Ushio
 end
 
 Ushio.cat
-
-module CatTower
-		
-	def col
-		image = Magick::ImageList.new("octopass.png")
-		puts
-		print "画像の幅 : "
-		puts image.columns
-		puts
-		print "画像の高さ : "
-		puts image.rows
-		puts
-	end
-
-	def pix
-		image = Magick::ImageList.new("octopass.png")
-		puts "ピクセル情報の取得"
-		pixels = image.get_pixels(0, 0, image.columns, image.rows)
-		" 赤 : "
-		pixels[0].red
-		puts
-		print " 緑 : "
-		puts pixels[0].green
-		puts
-		print " 青 : "
-		puts pixels[0].blue
-		puts
-		print " [色相, 彩度, 明度, 透明度] : "
-		print pixels[0].to_hsla
-		puts
-	end
-	module_function :col,:pix
-end
-
-CatTower.col
-CatTower.pix
---
+++ rmagick2.rb
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+require 'RMagick'
+include Magick
+
+module CatTower
+
+	def col
+		image = Magick::ImageList.new("CatTera.png")
+		puts
+		print "画像の幅 : "
+		puts image.columns
+		puts
+		print "画像の高さ : "
+		puts image.rows
+		puts
+	end
+
+	def pix
+		image = Magick::ImageList.new("CatTera.png")
+		puts "ピクセル情報の取得"
+		pixels = image.get_pixels(0, 0, image.columns, image.rows)
+        puts
+        print " 赤 : "
+		puts pixels[0].red
+		puts
+		print " 緑 : "
+		puts pixels[0].green
+		puts
+		print " 青 : "
+		puts pixels[0].blue
+		puts
+		print " [色相, 彩度, 明度, 透明度] : "
+		print pixels[0].to_hsla
+		puts
+	end
+	module_function :col,:pix
+end
+
+CatTower.col
+CatTower.pix
