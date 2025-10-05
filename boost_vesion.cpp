7036cc287f32d6e24e02fd721de98ef549c08243
T・K <karuma.reason@gmail.com>
2025-10-05 11:00:39 +0900

    Default.

---
+++ boost_vesion.cpp
@@ -0,0 +1,14 @@
+//
+// Copy by Takayuki Kamiyama on 2025/10/05.
+// https://boostjp.github.io/tips/version.html
+// 本サイトの情報は、 クリエイティブ・コモンズ 表示 3.0 非移植 ライセンス(CC BY) の下に提供されています。
+// https://creativecommons.org/licenses/by/3.0/deed.ja
+// ※ MITライセンスではありません。
+//
+
+#include "all.h"
+
+int main() {
+    cout << BOOST_VERSION << endl; // 108900
+        cout << BOOST_LIB_VERSION << endl; // 1_89
+}
